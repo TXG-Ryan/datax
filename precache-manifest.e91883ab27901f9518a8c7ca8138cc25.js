3dfcc4c9f57e42dd8f55ded200bd06922f67f4eb
TXG-Ryan <ryan.brown@txg.ie>
2022-03-07 15:47:20 +0000

    "gh-pages"

---
+++ precache-manifest.e91883ab27901f9518a8c7ca8138cc25.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c007da70067607b872",
+    "url": "/datax/css/app.8fb328b9.css"
+  },
+  {
+    "revision": "ff12b540fafc31d5e53e2e9dd73942ca",
+    "url": "/datax/img/arrow-down-right.ff12b540.svg"
+  },
+  {
+    "revision": "0356d07db2af76507cad8e9766758085",
+    "url": "/datax/img/arrow-up-right.0356d07d.svg"
+  },
+  {
+    "revision": "6f7280b1c4a1ab2f9039b3cd0f49e89d",
+    "url": "/datax/img/logo.6f7280b1.png"
+  },
+  {
+    "revision": "b274c4f2464cf22d692183d3f74ce931",
+    "url": "/datax/img/on-fire.b274c4f2.gif"
+  },
+  {
+    "revision": "a23005d668f684678c0a55df0fb4fac9",
+    "url": "/datax/index.html"
+  },
+  {
+    "revision": "9fb84c4bcfa637808ee0",
+    "url": "/datax/js/about.d1f41a02.js"
+  },
+  {
+    "revision": "b9c007da70067607b872",
+    "url": "/datax/js/app.46f0a435.js"
+  },
+  {
+    "revision": "c64ed8719f21fedf6a66",
+    "url": "/datax/js/chunk-vendors.1747fc21.js"
+  },
+  {
+    "revision": "690c84f64c05d70f187cbee7542f7382",
+    "url": "/datax/local.css"
+  },
+  {
+    "revision": "ecfb0c8bc1bab7fba83b481189ddb2c3",
+    "url": "/datax/manifest.json"
+  },
+  {
+    "revision": "cf155365a13a39380747bdb087393bbe",
+    "url": "/datax/masterStyles.css"
+  },
+  {
+    "revision": "afb4942e5838a7dc3b63d00ffcc87c9d",
+    "url": "/datax/normalize.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/datax/robots.txt"
+  }
+]);
